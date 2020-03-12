2340b525cc781cc573a827edfa1366c33bfdbd63
Vinodh Samuel <samuelvinodh@gmail.com>
2020-03-12 14:06:32 -0400

    REST Services: Content Negotiation & Swagger

--- restful-web-services/src/main/java/com/sam/rest/webservices/model/User.java
+++ restful-web-services/src/main/java/com/sam/rest/webservices/model/User.java
@@ -1,14 +1,20 @@
 package com.sam.rest.webservices.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "Social Media User entity")
 public class User {
     private Integer id;
     @Size(min=2, message = "Name should have atleast 2 characters")
+    @ApiModelProperty(notes="Name should have atleast 2 characters")
     private String name;
     @Past
+    @ApiModelProperty(notes="Birth Date must be in past")
     private Date birthDate;
 
     public User() {
